e9ddf039243aca6287fcd67340387ba5b4cbcf4f
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-09-18 11:31:13 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/jetpackcompose02_2025/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose02_2025/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose02_2025.ui.theme.JetPackCompose02_2025Theme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,16 +33,63 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    MyLayout()
+}
+
+@Composable
+fun MyLayout() {
+    Column(Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Green)) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)) {
+
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color.Blue)) {
+            }
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                            .background(Color.DarkGray)) {
+                    }
+                    Box(
+                        Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                            .background(Color.White),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Box Blanco")
+                    }
+                }
+            }
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Red)) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetPackCompose02_2025Theme {
-        Greeting("Android")
-    }
+    MyLayout()
 }
